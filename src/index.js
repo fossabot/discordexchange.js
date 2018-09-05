0a7119ab31d35f5ac0f37e2f40d53e35d52f1a94
Charalampos Fanoulis <charalampos.fanoulis@gmail.com>
2018-09-05 23:04:20 +0300

    Message handling is now a thing :dabbitThugLife:

--- src/index.js
+++ src/index.js
@@ -1,28 +1,48 @@
 const websocket = require("ws");
 const EventEmitter = require("events");
 const ws = new websocket("https://gateway.discord.exchange");
+var currencies; //eslint-disable-line
 class ExchangeClient extends EventEmitter {
     constructor(token, botID) {
         super();
         this.token = token;
         this.botID = botID;
     }
-    authenticate () {
-        var sendData = JSON.stringify({"op": 1, "data": {"token": this.token}});
+    authenticate() {
+        var sendData = JSON.stringify({
+            "op": 1,
+            "data": {
+                "token": this.token
+            }
+        });
         ws.send(sendData);
     }
-    initiateTransaction (user, recipient, amout) { //eslint-disable-line no-unused-vars
+    initiateTransaction(user, recipient, amout) { //eslint-disable-line no-unused-vars
 
     }
 }
-async function messageHandler(data) {
-    JSON.parse(data);
 
+function messageHandler(data) {
+    var oData = JSON.parse(data);
+    var op = oData.op;
+    switch (op) {
+        case 1:
+            if (!oData.d.success) {
+                const err = oData.d.message;
+                ExchangeClient.emit("AuthUnsuccessful", err);
+                break;
+            }
+            ExchangeClient.emit("AuthSuccessful");
+            break;
+        default:
+            ExchangeClient.emit("UnknownMessage");
+            break;
+    }
 }
+
 ws.on("message", function (data) {
     messageHandler(data);
 });
 
 
 module.exports = ExchangeClient;
-
