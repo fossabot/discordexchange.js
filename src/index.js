1214725ac5d9f618f87bb10203a0ee86002e1789
Charalampos Fanoulis <charalampos.fanoulis@gmail.com>
2018-09-07 16:25:53 +0300

    refactor(index.js): Change the way authentication is handled

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const websocket = require("ws");
 const EventEmitter = require("events");
+
+//Load events
+const auth = require("./functions/auth");
+
 const ws = new websocket("https://gateway.discord.exchange");
 var currencies; //eslint-disable-line
 class ExchangeClient extends EventEmitter {
@@ -9,40 +13,16 @@ class ExchangeClient extends EventEmitter {
         this.botID = botID;
     }
     authenticate() {
-        var sendData = JSON.stringify({
-            "op": 1,
-            "data": {
-                "token": this.token
-            }
+        return new Promise ( (resolve, reject) => {
+            auth(ws, this.token, this.botID)
+            .then(data => resolve(data))
+            .catch(err => reject(err));
         });
-        ws.send(sendData);
+        
     }
     initiateTransaction(user, recipient, amout) { //eslint-disable-line no-unused-vars
 
     }
 }
-
-function messageHandler(data) {
-    var oData = JSON.parse(data);
-    var op = oData.op;
-    switch (op) {
-        case 1:
-            if (!oData.d.success) {
-                const err = oData.d.message;
-                ExchangeClient.emit("AuthUnsuccessful", err);
-                break;
-            }
-            ExchangeClient.emit("AuthSuccessful");
-            break;
-        default:
-            ExchangeClient.emit("UnknownMessage");
-            break;
-    }
-}
-
-ws.on("message", function (data) {
-    messageHandler(data);
-});
-
 
 module.exports = ExchangeClient;
