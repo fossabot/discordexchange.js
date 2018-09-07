1e5b5e404ccdac4bb250ecf14616acd962f539ee
Charalampos Fanoulis <charalampos.fanoulis@gmail.com>
2018-09-07 14:12:59 +0300

    refactor(Client authentication): Exported auth function to a seperate file
    BREAKING CHANGE: The auth function is in a new file
    
    Signed-off-by: Charalampos Fanoulis <charalampos.fanoulis@gmail.com>

---
+++ src/functions/auth.js
@@ -0,0 +1,24 @@
+module.exports = function (ws, token, botID, timeout = 20000) {
+    return new Promise( (resolve, reject) => {
+            const toSend = {
+                "op": 1,
+                "d": {
+                    "token": token,
+                    "botID": botID
+                }
+            };
+            ws.send(JSON.stringify(toSend));
+            const timeoutTimer = setTimeout(() => {
+                reject("Timed out");
+            }, timeout);
+
+            ws.on("message", function(data) {
+                const received = JSON.parse(data);
+                if (received.op == 1 && received.data.success == true){
+                    clearTimeout(timeoutTimer);
+                    return resolve(received.d);
+                }
+            });
+        }
+    );
+};
